a48d5b1eacd138c8a83829ebefc956cb5889498d
mansoor-nouman <54888881+mansoor-nouman@users.noreply.github.com>
2021-04-20 22:03:51 +0530

    Add files via upload

--- src/api.js
+++ src/api.js
@@ -1,25 +1,25 @@
 import axios from "axios";
 
 export function Postregister(data){
-    return axios.post(`https://urlshortmansoor.herokuapp.com//register`,data)
+    return axios.post(`https://urlshortmansoor.herokuapp.com/register`,data)
 }
 
 export function Postlogin(data){
-    return axios.post(`https://urlshortmansoor.herokuapp.com//login`,data)
+    return axios.post(`https://urlshortmansoor.herokuapp.com/login`,data)
 }
 
 export function Postlongurl(data){
-    return axios.post(`https://urlshortmansoor.herokuapp.com//urlshort`,data)
+    return axios.post(`https://urlshortmansoor.herokuapp.com/urlshort`,data)
 }
 
 export function Getlongurl(){
-    return axios.get(`https://urlshortmansoor.herokuapp.com//urlshort`)
+    return axios.get(`https://urlshortmansoor.herokuapp.com/urlshort`)
 }
 
 export function GetlongurlbyEmail(email){
-    return axios.get(`https://urlshortmansoor.herokuapp.com//userurl/${email}`)
+    return axios.get(`https://urlshortmansoor.herokuapp.com/userurl/${email}`)
 }
 
 export function Getlongurlbyid(id){
-    return axios.get(`https://urlshortmansoor.herokuapp.com//${id}`)
+    return axios.get(`https://urlshortmansoor.herokuapp.com/${id}`)
 }
